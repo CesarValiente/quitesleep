97cb4dcabd1c6c267ff65f52bd419884d4c61226
Cesar Valiente <cesar@6wunderkinder.com>
2013-11-02 10:51:33 +0100

    * Come back to work :-)

--- quitesleep3/src/es/cesar/quitesleep/ui/activities/MailSettings.java
+++ quitesleep3/src/es/cesar/quitesleep/ui/activities/MailSettings.java
@@ -15,7 +15,7 @@
 
     You should have received a copy of the GNU General Public License
     along with QuiteSleep.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package es.cesar.quitesleep.ui.activities;
 
@@ -24,7 +24,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -32,6 +31,7 @@ import android.widget.ToggleButton;
 import com.actionbarsherlock.app.SherlockDialogFragment;
 
 import es.cesar.quitesleep.R;
+import es.cesar.quitesleep.components.interfaces.ICallbacks;
 import es.cesar.quitesleep.data.controllers.ClientDDBB;
 import es.cesar.quitesleep.data.models.Settings;
 import es.cesar.quitesleep.operations.DialogOperations;
@@ -45,164 +45,163 @@ import es.cesar.quitesleep.utils.ExceptionUtils;
  * 
  * @author Cesar Valiente Gordo
  * @mail cesar.valiente@gmail.com
- *
+ * 
  */
-public class MailSettings extends BaseFragmentActivity implements OnClickListener {
-	
-	private final String CLASS_NAME = getClass().getName();	
-	
-	//Widgets id's
-	private final int userEditTextId = R.id.mailsettings_edittext_user;
-	private final int passwdEditTextId = R.id.mailsettings_edittext_passwd;
-	private final int subjectEditTextId = R.id.mailsettings_edittext_subject;
-	private final int bodyEditTextId = R.id.mailsettings_edittext_body;
-	private final int saveMailButtonId = R.id.mailsettings_button_savemail;	
-	private final int mailServiceToggleButtonId = R.id.mailsettings_togglebutton_mailservice;	
-	
-	//Widgets
-	private EditText userEditText;
-	private EditText passwdEditText;
-	private EditText subjectEditText;
-	private EditText bodyEditText;
-	private Button saveMailButton;
-	private ToggleButton mailServiceToggleButton;		
-	
-	
-	
-	@Override
-	public void onCreate (Bundle savedInstanceState) {				
-					
-		super.onCreate(savedInstanceState);
-		super.homeToUp(true);						
-		
-		setContentView(R.layout.mailsettings);
-		
-		//Set the widgets elements
-		userEditText = (EditText)findViewById(userEditTextId);
-		passwdEditText = (EditText)findViewById(passwdEditTextId);
-		subjectEditText = (EditText)findViewById(subjectEditTextId);
-		bodyEditText = (EditText)findViewById(bodyEditTextId);
-		saveMailButton = (Button)findViewById(saveMailButtonId);
-		mailServiceToggleButton = (ToggleButton)findViewById(mailServiceToggleButtonId);			
-		
-		//Set OnClickListener events
-		saveMailButton.setOnClickListener(this);	
-		mailServiceToggleButton.setOnClickListener(this);
-	
-		//Put in the widgets the prevoious data saved into ddbb.
-		getDefaultValues();				
-	}
-	
-	
-	@Override
-	public void onClick (View view) {
-		
-		int viewId = view.getId();					
-		
-		switch (viewId) {
-			case saveMailButtonId:				
-				prepareSaveMailOperation();
-				break;			
-			
-			case mailServiceToggleButtonId:
-				if (mailServiceToggleButton.isChecked()) {
-					FragmentTransaction ft = getSupportFragmentManager().beginTransaction();					
-					SherlockDialogFragment dialog = SmsEmailFragmentDialog.newInstance(
-							ConfigAppValues.DialogType.MAIL_DIALOG);
-					dialog.show(ft, "dialog");
-				}else
-					DialogOperations.checkMailService(
-							this,
-							mailServiceToggleButton.isChecked());
-				break;						
-				
-			default:
-				break;
-		}
-	}
+public class MailSettings extends BaseFragmentActivity implements
+        OnClickListener, ICallbacks.SaveData {
 
-	
-	/**
-	 * Function that put in the widgets the data saved into ddbb.
-	 */
-	private void getDefaultValues () {
-		
-		try {
-			
-			ClientDDBB clientDDBB = new ClientDDBB();
-			Settings settings = clientDDBB.getSelects().selectSettings();
-			
-			if (settings != null) {
-				
-				if (settings.getUser() != null && !settings.getUser().equals(""))
-					userEditText.setText(settings.getUser());
-				if (settings.getPasswd() != null && !settings.getPasswd().equals(""))
-					passwdEditText.setText(settings.getPasswd());
-				if (settings.getSubject() != null && !settings.getSubject().equals(""))
-					subjectEditText.setText(settings.getSubject());
-				if (settings.getBody() != null && !settings.getBody().equals(""))
-					bodyEditText.setText(settings.getBody());
-				
-				mailServiceToggleButton.setChecked(settings.isMailService());
-			} 
-			//If Settings object haven't been created previously, here we create.
-			else {
-				settings = new Settings(false);
-				
-				/* Save the mail settings, only the subject and the body if the
-				 * Settings object haven't been created for have something for
-				 * default attributes, user and passwd not because is important
-				 * for the mail send, and the user soon or later will have set this.
-				 */
-				settings.setSubject(subjectEditText.getText().toString());
-				settings.setBody(bodyEditText.getText().toString());
-								
-				clientDDBB.getInserts().insertSettings(settings);
-				clientDDBB.commit();
-			}
-			
-			clientDDBB.close();
-			
-		}catch (Exception e) {
-			Log.e(CLASS_NAME, ExceptionUtils.getString(e));
-		}
-	}
-	
-	/**
-	 * Prepare all data from widgets for call to the function that save data
-	 * into ddbb in Settings object
-	 *
-	 */
-	private void prepareSaveMailOperation () {
-		
-		try {
-			
-			String user 		= userEditText.getText().toString();
-			String passwd 		= passwdEditText.getText().toString();
-			String subject 		= subjectEditText.getText().toString();
-			String body 		= bodyEditText.getText().toString();
-			
-			//---- Log traces ---//
-			Log.d(CLASS_NAME, "user: " + user);
-			Log.d(CLASS_NAME, "passwd: " + passwd);
-			Log.d(CLASS_NAME, "subject: " + subject);
-			Log.d(CLASS_NAME, "body: " + body);
-			//------------------//
-						
-			if (MailOperations.saveMailSettings(user, passwd, subject, body))
-				//All right, start the service was ok!
-				es.cesar.quitesleep.utils.Toast.r(
-                		this,
-                		this.getString(
-                				R.string.mailsettings_toast_save),
-                		Toast.LENGTH_SHORT);
-			
-			
-		}catch (Exception e) {
-			Log.e(CLASS_NAME, ExceptionUtils.getString(e));
-		}
-	}
-	
-	
+    private final String CLASS_NAME = getClass().getName();
+
+    // Widgets
+    private EditText userEditText;
+    private EditText passwdEditText;
+    private EditText subjectEditText;
+    private EditText bodyEditText;
+    private ToggleButton mailServiceToggleButton;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        super.homeToUp(true);
+
+        setContentView(R.layout.mailsettings);
+        getSupportActionBar().setTitle(
+                getString(R.string.mailsettings_actionbar_title));
+
+        // Set the widgets elements
+        userEditText = (EditText) findViewById(R.id.mailsettings_edittext_user);
+        passwdEditText = (EditText) findViewById(R.id.mailsettings_edittext_passwd);
+        subjectEditText = (EditText) findViewById(R.id.mailsettings_edittext_subject);
+        bodyEditText = (EditText) findViewById(R.id.mailsettings_edittext_body);
+        mailServiceToggleButton = (ToggleButton) findViewById(R.id.mailsettings_togglebutton_mailservice);
+
+        // Set OnClickListener events
+        mailServiceToggleButton.setOnClickListener(this);
+
+        // Put in the widgets the prevoious data saved into ddbb.
+        getDefaultValues();
+    }
+
+    @Override
+    public void onClick(final View view) {
+
+        int viewId = view.getId();
+
+        switch (viewId) {
+            case R.id.mailsettings_togglebutton_mailservice:
+                if (mailServiceToggleButton.isChecked()) {
+                    FragmentTransaction ft = getSupportFragmentManager()
+                            .beginTransaction();
+                    SherlockDialogFragment dialog = SmsEmailFragmentDialog
+                            .newInstance(
+                                    ConfigAppValues.DialogType.MAIL_DIALOG,
+                                    this);
+                    dialog.show(ft, "dialog");
+                } else {
+                    DialogOperations.checkMailService(this,
+                            mailServiceToggleButton.isChecked());
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Function that put in the widgets the data saved into ddbb.
+     */
+    private void getDefaultValues() {
+
+        try {
+
+            ClientDDBB clientDDBB = new ClientDDBB();
+            Settings settings = clientDDBB.getSelects().selectSettings();
+
+            if (settings != null) {
+
+                if (settings.getUser() != null
+                        && !settings.getUser().equals("")) {
+                    userEditText.setText(settings.getUser());
+                }
+                if (settings.getPasswd() != null
+                        && !settings.getPasswd().equals("")) {
+                    passwdEditText.setText(settings.getPasswd());
+                }
+                if (settings.getSubject() != null
+                        && !settings.getSubject().equals("")) {
+                    subjectEditText.setText(settings.getSubject());
+                }
+                if (settings.getBody() != null
+                        && !settings.getBody().equals("")) {
+                    bodyEditText.setText(settings.getBody());
+                }
+
+                mailServiceToggleButton.setChecked(settings.isMailService());
+            }
+            // If Settings object haven't been created previously, here we
+            // create.
+            else {
+                settings = new Settings(false);
+
+                /*
+                 * Save the mail settings, only the subject and the body if the
+                 * Settings object haven't been created for have something for
+                 * default attributes, user and passwd not because is important
+                 * for the mail send, and the user soon or later will have set
+                 * this.
+                 */
+                settings.setSubject(subjectEditText.getText().toString());
+                settings.setBody(bodyEditText.getText().toString());
+
+                clientDDBB.getInserts().insertSettings(settings);
+                clientDDBB.commit();
+            }
+
+            clientDDBB.close();
+
+        } catch (Exception e) {
+            Log.e(CLASS_NAME, ExceptionUtils.getString(e));
+        }
+    }
+
+    /**
+     * Prepare all data from widgets for call to the function that save data
+     * into ddbb in Settings object
+     * 
+     */
+    private void prepareSaveMailOperation() {
+
+        try {
+
+            String user = userEditText.getText().toString();
+            String passwd = passwdEditText.getText().toString();
+            String subject = subjectEditText.getText().toString();
+            String body = bodyEditText.getText().toString();
+
+            // ---- Log traces ---//
+            Log.d(CLASS_NAME, "user: " + user);
+            Log.d(CLASS_NAME, "passwd: " + passwd);
+            Log.d(CLASS_NAME, "subject: " + subject);
+            Log.d(CLASS_NAME, "body: " + body);
+            // ------------------//
+
+            if (MailOperations.saveMailSettings(user, passwd, subject, body)) {
+                // All right, start the service was ok!
+                es.cesar.quitesleep.utils.Toast.r(this,
+                        this.getString(R.string.mailsettings_toast_save),
+                        Toast.LENGTH_SHORT);
+            }
+
+        } catch (Exception e) {
+            Log.e(CLASS_NAME, ExceptionUtils.getString(e));
+        }
+    }
+
+    @Override
+    public void saveEmailSMSConfig() {
+        prepareSaveMailOperation();
+    }
 
 }
--- quitesleep3/src/es/cesar/quitesleep/ui/activities/SmsSettings.java
+++ quitesleep3/src/es/cesar/quitesleep/ui/activities/SmsSettings.java
@@ -15,7 +15,7 @@
 
     You should have received a copy of the GNU General Public License
     along with QuiteSleep.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package es.cesar.quitesleep.ui.activities;
 
@@ -24,7 +24,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -32,6 +31,7 @@ import android.widget.ToggleButton;
 import com.actionbarsherlock.app.SherlockDialogFragment;
 
 import es.cesar.quitesleep.R;
+import es.cesar.quitesleep.components.interfaces.ICallbacks;
 import es.cesar.quitesleep.data.controllers.ClientDDBB;
 import es.cesar.quitesleep.data.models.Settings;
 import es.cesar.quitesleep.operations.DialogOperations;
@@ -45,124 +45,120 @@ import es.cesar.quitesleep.utils.ExceptionUtils;
  * 
  * @author Cesar Valiente Gordo
  * @mail cesar.valiente@gmail.com
- *
+ * 
  */
-public class SmsSettings extends BaseFragmentActivity implements OnClickListener {
-	
-	final private String CLASS_NAME = getClass().getName();	
-	
-	//Ids for widgets
-	final int smsEditTextId = R.id.smssettings_edittext_savesms;
-	final int saveSmsButtonId = R.id.smssettings_button_savesms;	
-	final int smsServiceToggleButtonId = R.id.smssettings_togglebutton_smsservice;	
-	
-	//Widgets
-	private EditText smsEditText;
-	private Button saveSmsButton;
-	private ToggleButton smsServiceToggleButton;		
-	
-	
-	public void onCreate (Bundle savedInstanceState) {				
-					
-		super.onCreate(savedInstanceState);
-		super.homeToUp(true);
-				
-		setContentView(R.layout.smssettings);
-					
-		smsEditText = (EditText)findViewById(smsEditTextId);
-		saveSmsButton = (Button)findViewById(saveSmsButtonId);
-		smsServiceToggleButton = (ToggleButton)findViewById(smsServiceToggleButtonId);		
-		
-		saveSmsButton.setOnClickListener(this);
-		smsServiceToggleButton.setOnClickListener(this);			
-		
-		
-		//Put in the widgets the prevoious data saved into ddbb.
-		getDefaultValues();				
-	}
-	
-		
-	@Override
-	public void onClick (View view) {
-		
-		int viewId = view.getId();
-				
-		switch (viewId) {
-		
-			case saveSmsButtonId:				
-				prepareSaveSmsOperation();
-				break;				
-			case smsServiceToggleButtonId:
-				if (smsServiceToggleButton.isChecked()) { 														
-					FragmentTransaction ft = getSupportFragmentManager().beginTransaction();						
-					SherlockDialogFragment dialog = SmsEmailFragmentDialog.newInstance(ConfigAppValues.DialogType.SMS_DIALOG);
-					dialog.show(ft, "dialog");
-				}else 					
-					DialogOperations.checkSmsService(
-							this, 
-							smsServiceToggleButton.isChecked());												
-				break;							
-				
-			default:
-				break;
-		}						
-	}
-			
-	/**
-	 * Put the default values saved in the ddbb in the widgets
-	 */
-	private void getDefaultValues () {
-		
-		try {
-			ClientDDBB clientDDBB = new ClientDDBB();
-			Settings settings = clientDDBB.getSelects().selectSettings();
-			
-			if (settings != null) {
-				if (settings.getSmsText()!= null && !settings.getSmsText().equals(""))
-					smsEditText.setText(settings.getSmsText());
-				
-				smsServiceToggleButton.setChecked(settings.isSmsService());
-				
-			}else {
-				settings = new Settings(false);
-				clientDDBB.getInserts().insertSettings(settings);
-				
-				/* Save the sms text in the settings if the Settings object haven't
-				 * been created, so the predefined text will be, for the moment,
-				 * the sms text in the settings object
-				 */
-				settings.setSmsText(smsEditText.getText().toString());
-				clientDDBB.commit();
-			}
-			
-			clientDDBB.close();
-			
-		}catch (Exception e) {
-			Log.e(CLASS_NAME, ExceptionUtils.getString(e));
-		}
-	}
-	
-	/**
-	 * Function that prepare the data for save into ddbb and call to the function
-	 * that does the operation.
-	 */
-	private void prepareSaveSmsOperation () {
-		
-		try {
-			
-			String smsText = smsEditText.getText().toString();						
-			
-			if (SmsOperations.saveSmsSettings(smsText))
-				es.cesar.quitesleep.utils.Toast.r(
-                		this,
-                		this.getString(
-                				R.string.smssettings_toast_save),
-                		Toast.LENGTH_SHORT);
-			
-		}catch (Exception e) {
-			Log.e(CLASS_NAME, ExceptionUtils.getString(e));
-		}
-	}
-	
+public class SmsSettings extends BaseFragmentActivity implements
+        OnClickListener, ICallbacks.SaveData {
+
+    final private String CLASS_NAME = getClass().getName();
+
+    // Widgets
+    private EditText smsEditText;
+    private ToggleButton smsServiceToggleButton;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        super.homeToUp(true);
+
+        setContentView(R.layout.smssettings);
+        getSupportActionBar().setTitle(
+                getString(R.string.smssettings_actionbar_title));
+
+        smsEditText = (EditText) findViewById(R.id.smssettings_edittext_savesms);
+        smsServiceToggleButton = (ToggleButton) findViewById(R.id.smssettings_togglebutton_smsservice);
+
+        smsServiceToggleButton.setOnClickListener(this);
+
+        // Put in the widgets the prevoious data saved into ddbb.
+        getDefaultValues();
+    }
+
+    @Override
+    public void onClick(final View view) {
+
+        switch (view.getId()) {
+
+            case R.id.smssettings_togglebutton_smsservice:
+                if (smsServiceToggleButton.isChecked()) {
+                    FragmentTransaction ft = getSupportFragmentManager()
+                            .beginTransaction();
+                    SherlockDialogFragment dialog = SmsEmailFragmentDialog
+                            .newInstance(ConfigAppValues.DialogType.SMS_DIALOG,
+                                    this);
+                    dialog.show(ft, "dialog");
+                } else {
+                    DialogOperations.checkSmsService(this,
+                            smsServiceToggleButton.isChecked());
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void saveEmailSMSConfig() {
+        prepareSaveSmsOperation();
+    }
+
+    /**
+     * Put the default values saved in the ddbb in the widgets
+     */
+    private void getDefaultValues() {
+
+        try {
+            ClientDDBB clientDDBB = new ClientDDBB();
+            Settings settings = clientDDBB.getSelects().selectSettings();
+
+            if (settings != null) {
+                if (settings.getSmsText() != null
+                        && !settings.getSmsText().equals("")) {
+                    smsEditText.setText(settings.getSmsText());
+                }
+
+                smsServiceToggleButton.setChecked(settings.isSmsService());
+
+            } else {
+                settings = new Settings(false);
+                clientDDBB.getInserts().insertSettings(settings);
+
+                /*
+                 * Save the sms text in the settings if the Settings object
+                 * haven't been created, so the predefined text will be, for the
+                 * moment, the sms text in the settings object
+                 */
+                settings.setSmsText(smsEditText.getText().toString());
+                clientDDBB.commit();
+            }
+
+            clientDDBB.close();
+
+        } catch (Exception e) {
+            Log.e(CLASS_NAME, ExceptionUtils.getString(e));
+        }
+    }
+
+    /**
+     * Function that prepare the data for save into ddbb and call to the
+     * function that does the operation.
+     */
+    private void prepareSaveSmsOperation() {
+
+        try {
+
+            String smsText = smsEditText.getText().toString();
+
+            if (SmsOperations.saveSmsSettings(smsText)) {
+                es.cesar.quitesleep.utils.Toast.r(this,
+                        this.getString(R.string.smssettings_toast_save),
+                        Toast.LENGTH_SHORT);
+            }
+
+        } catch (Exception e) {
+            Log.e(CLASS_NAME, ExceptionUtils.getString(e));
+        }
+    }
 
 }
